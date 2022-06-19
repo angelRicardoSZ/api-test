3365e391d028ae84047a1cdb7ab118aea747958e
angelRicardoSZ <angel.sanchezzefe@alumno.buap.mx>
2022-06-18 22:22:01 -0500

    deleting routes

--- routes/index.js
+++ routes/index.js
@@ -7,8 +7,8 @@ function routerApi(app) {
   const router = express.Router();
   app.use("/api/v1", router)
   router.use("/products", productsRouter)
-  router.use("/users", usersRouter)
-  router.use("/categories", categoriesRouter)
+  // router.use("/users", usersRouter)
+  // router.use("/categories", categoriesRouter)
 
 }
 
